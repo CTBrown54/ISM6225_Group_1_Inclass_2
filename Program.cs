1335a4ec8c8f95ede8977282d24580af7e34af00
CTBrown54 <CTBrown54@yahoo.com>
2025-03-25 16:26:49 -0400

    Update Program.cs
    Added book class and data

--- Program.cs
+++ Program.cs
@@ -11,48 +11,52 @@ namespace Library_Management_System
         public string ID;
 
     }
-/*
-       public Person()
-        {
-           Name = "";
-            Email = "Name + @usf.edu";
-            ID = "U123456";
-        }
-*/
 
     class Student: Person
     {
         public string Major;
         public string GraduationYear;
     }
-     /*   public Student(): base()
-        {
-            Major = "BAIS";
-            GraduationYear = "2026";
-        }
 
-    */
     class Staff : Person
     {
         public string Position;
         public string Department;
     }
-     /*   public Staff() : base()
-        {
-            Position = "Professor";
-            Department = "BAIS";
-        }
+
+    class book
+	{
+		public string title;
+		public string author;
+		public string isbn;
+		public int availCopies;
+
+		public string BorrowBook()
+		{
+			return title;
+		}
     }
-     */
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-
             Student S1 = new Student { Name = "Akhil", Email = "akhil@usf.edu", ID = "S001", Major = "Business Analytics", GraduationYear = "2026" };
             Student S2 = new Student { Name = "Sandeep", Email = "sandeep@usf.edu", ID = "S002", Major = "Information Systems", GraduationYear = "2025" };
             Staff ST1 = new Staff { Name = "Grandon Gill", Email = "grandon@usf.edu", ID = "ST001", Position = "Librarian", Department = "Library Services" };
+
+            book b1 = new book { title = "The Art of Data Strategy", author = "Liam Reynolds", isbn = "ISBN111", availCopies = 4 };
+			book b2 = new book { title = "usiness Insights with AI", author = "Olivia Carter", isbn = "ISBN222", availCopies = 3 };
+			book b3 = new book { title = "Analytics in Action", author = "Nathan Brooks", isbn = "ISBN333", availCopies = 6 };
+
+			Console.WriteLine("Books in Library:");
+			Console.WriteLine("Title: " + b1.title + ", Author: " + b1.author + ", Available Copies: " + b1.availCopies);
+			Console.WriteLine("Title: " + b2.title + ", Author: " + b2.author + ", Available Copies: " + b2.availCopies);
+			Console.WriteLine("Title: " + b3.title + ", Author: " + b3.author + ", Available Copies: " + b3.availCopies);
+
+			Console.WriteLine();
+			Console.WriteLine("Borrowing Books...");
+			Console.WriteLine("borrowed " + b1.BorrowBook());
         }
     }
 }
